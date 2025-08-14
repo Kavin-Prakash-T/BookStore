1f8344d23d341ef125297eb450c88b0e807da0f9
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-08-14 23:20:43 +0530

    login and logout feature

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"; 
 export const PageNotFound = () => {
     return (
         <main>
@@ -9,9 +10,9 @@ export const PageNotFound = () => {
                     </div>
                 </div>
                 <div className="flex justify-center my-4">
-                    <a href="">
+                    <Link to="/">
                         <button type="button" className="w-64 text-2xl text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br font-medium rounded-lg px-5 py-2.5 text-center mr-2 mb-2">Back To Home</button>
-                    </a>
+                    </Link>
                 </div>
             </section>
         </main>
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage, ProductsList, ProductDetail, Login, Register } from "../pages";
+import { HomePage, ProductsList, ProductDetail, Login, Register, PageNotFound } from "../pages";
 
 export const AllRoutes = () => {
   return (
@@ -10,6 +10,7 @@ export const AllRoutes = () => {
         <Route path="/products/:id" element={<ProductDetail />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </>
   );
