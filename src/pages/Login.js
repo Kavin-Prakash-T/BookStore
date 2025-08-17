e16bcfcea16c27334de6d965d566e4db89f7a2f1
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-08-17 17:44:29 +0530

    login as guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -24,6 +24,17 @@ export const Login = () => {
       toast.error(error.message);
     }
   }
+  async function handleLoginasguest(){
+   email.current.value=process.env.REACT_APP_GUEST_LOGIN;
+   password.current.value=process.env.REACT_APP_GUEST_PASSWORD;
+   try{
+   const data=await login({email:email.current.value,password:password.current.value});
+   data.accessToken?navigate("/products"):toast.error(data);
+   }
+   catch(error){
+    toast.error(error.message);
+   }
+  }
   return (
     <main>
       <section>
@@ -71,7 +82,7 @@ export const Login = () => {
           Log In
         </button>
       </form>
-      {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+      <button onClick={handleLoginasguest}className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> 
     </main>
   );
 };
